a555f05005fd88ae6ef4045e0f64e7533527a539
Harshad <72267094+harsh044@users.noreply.github.com>
2025-04-16 10:55:31 +0530

    handle refreshing..

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path');
 const app = express();
 
 // packages
@@ -34,6 +35,14 @@ app.use(
     })
 )
 
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+// Handle SPA
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+});
+
+app.listen(3000, () => console.log('Server running......'));
 
 const PORT = process.env.PORT || 5000;
 
